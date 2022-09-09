506cc128907c045d0b3ce152e59f465b37d6dfb1
accuaccu <paavo.raittinen@gmail.com>
2022-09-09 19:47:13 +0300

    Just a nice palette and background defined for plotting etc.

---
+++ colors.R
@@ -0,0 +1,26 @@
+p <- colorRampPalette(c('#0dec53','#2ac2cf'))(10)
+m <- colorRampPalette(c('#f90076','#f95b00'))(10)
+b <- '#000518'
+
+
+
+A <- 
+  matrix(
+    c(
+      x <- table(cut(rnorm(1000), breaks = 10)),
+      y <- table(cut(rnorm(1000), breaks = 10))
+    ),
+    ncol = 2,
+    byrow = FALSE
+  )
+
+par(bg = b, pty = 's', font.axis = 3, font.lab = 3, family = 'serif', col.axis = m[5], col.lab = m[5])
+barplot(t(A), beside = TRUE, col = c(p,m), border = FALSE, horiz = FALSE)
+
+par(mfrow = c(1,1))
+hist(rnorm(1000), col = m, col.lab = m[5], axes = F, main = '')
+hist(rnorm(1000), col = p, col.lab = p[5], axes = F, main = '')
+
+x <- density(rnorm(1000))
+plot(x, col = p, axes = FALSE)
+polygon(x, col = paste0(m,33), border = m)
